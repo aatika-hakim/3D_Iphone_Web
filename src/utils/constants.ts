9de1eda585ac1f8ade4b4cb4418dadea5103471f
Aatika hakim <aatikahakim90@gmail.com>
2024-06-25 13:07:30 +0500

    navlist routes

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const navlist = ["Store", "Mac", "iphone", "Support"];
+
+export const highLights = [
+    {
+        id: 1,
+        textLists: [
+            "Enter A17 Pro",
+            "Game-changing chip."
+            
+        ]
+    }
+]
