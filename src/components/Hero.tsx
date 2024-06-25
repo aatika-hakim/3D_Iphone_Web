79dcc808b86b04e0d592e5508e4e194da0349fae
Aatika hakim <aatikahakim90@gmail.com>
2024-06-25 13:14:00 +0500

    hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p className='text-center font-semibold text-3xl text-gray-300 opacity-0 max-md:mb-10'>
+          iphone 15 Pro
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
